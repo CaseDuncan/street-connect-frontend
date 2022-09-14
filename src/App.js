a8b86dd01753edf1aa2d6b0149b6d28e81c3438f
Duncan Oluoch <oluochdancun19@gmail.com>
2022-09-14 19:48:32 +0300

    removed the footer

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App({handleAddService}) {
       <Route path='/users/add_user' element={<NewUser/>}/>
       <Route path='*' element={<PageNotFound/>}/>
       </Routes>
-      {/* <Footer/> */}
+     
     </div>
   );
 }
